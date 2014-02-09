1791f05c98a0c11f1240d712c9174bd9a54b4aa7
Noitidart <noitidart@gmail.com>
2014-02-08 19:30:09 -0800

    initial
    bootstrap.js
    -based on fork of gist https://gist.github.com/Noitidart/8673632
    -had to use load to listenPageLoad because soudncloud does preloaders
    -learned: had to set addEventListener('load' 3rd param to true, as if
    false it wont listen on gBrowser
    -made copy of boostrap as good, now changing to so mut is within js
    wrapper because if reload page, it still finds regedMut[win] which is so
    weird, i thought on reload the win changes
    -todo: question: failed at getting mut into the contentwindow, but found
    a more efficient way (i think), create one mut per DOMWindow (the
    question i have, is when elements get destoryed, like if a tab is closed
    or navigated away from an element that the mut obs is connected to, what
    happens, does it get disconnected auto?)
    -on soundcloud website, if play something after it finishes it starts
    playing something else, so if i click right away on it, it creates some
    javascript endless loop that needs to be aborted, so put 100ms wait
    before clicking to play again
    
    install.rdf
    -icon artist name contributor
    -min max at 27
    -no options
    
    icon.png
    64px -
    http://www.iconarchive.com/show/the-circle-icons-by-xenatt/App-Soundcloud-icon.html)

---
+++ bootstrap good pre trying to make it all in contentwindow.js
@@ -0,0 +1,277 @@
+const {interfaces: Ci,	utils: Cu} = Components;
+Cu.import('resource://gre/modules/Services.jsm');
+
+var mutConfig = {attributes: true};
+function mutFunc(ms) {
+	for (var m of ms) {
+		Services.console.logStringMessage('SoundStorm - mut fired att changed = "' + m.attributeName + '""');
+		if (m.attributeName == 'class') {
+			var mClass = m.target.getAttribute('class');
+			Services.console.logStringMessage('SoundStorm - mClass Changed = "' + mClass + '""');
+			if (!/(pause|playing)/.test(mClass)) {
+				//click it to play it again
+				m.target.click();
+			}
+			break;
+		}
+	}
+}
+
+function mouseover(e) {
+	e.target.style.transform = 'scale(3,3)';
+}
+
+function mouseout(e) {
+	e.target.style.transform = '';
+}
+
+var regedMuts = {};
+
+function addDiv(doc) {
+	if (!doc) return;
+	if (!(doc instanceof Ci.nsIDOMHTMLDocument)) return;
+
+	var win = doc.defaultView;
+	var host = doc.location.host;
+	Services.console.logStringMessage('SoundStorm - host = "' + host + '""');
+	Services.console.logStringMessage('SoundStorm - isFrame = "' + win.frameElement + '""');
+
+	
+	switch(host) {
+		case 'w.soundcloud.com':
+			var selector = '.playButton';
+			var btns = doc.querySelectorAll(selector);
+			break;
+		case 'soundcloud.com':
+			var selector = '.sc-button-play';
+			var btns = doc.querySelectorAll(selector);		
+			break;
+		default:
+			return; //SoundStorm does not support other hosts
+	}
+	Services.console.logStringMessage('SoundStorm - ADD DIV');
+	Services.console.logStringMessage('SoundStorm - HOST NAME PASS');
+	
+	var js = win.wrappedJSObject;
+	if (!(win in regedMuts)) {
+	//if (!js.soundStormInited) {
+		//js.soundStormMut = new win.MutationObserver(mutFunc);
+		regedMuts[win] = new win.MutationObserver(mutFunc);
+		Services.console.logStringMessage('SoundStorm - MUT CREATED');
+	} else {
+		Services.console.logStringMessage('SoundStorm - MUT ALREADY IN regedMuts');
+	}
+	
+	Services.console.logStringMessage('SoundStorm - MUT if block passed but was it created?');
+	
+	Services.console.logStringMessage('SoundStorm - # of btns on this page = ' + btns.length);
+	[].forEach.call(btns, function(btn, i) {
+		Services.console.logStringMessage('SoundStorm - loop ' + i);
+		btn.addEventListener('mouseover', mouseover, false);
+		btn.addEventListener('mouseout', mouseout, false);
+		//js.soundStormMut.observe(btn, mutConfig);
+		regedMuts[win].observe(btn, mutConfig);
+	});
+	
+	Services.console.logStringMessage('SoundStorm - MOUSE EVENTS ADDED');
+	
+	Services.console.logStringMessage('SoundStorm - add success');
+}
+
+function removeDiv(doc) {
+	if (!doc) return;
+	if (!(doc instanceof Ci.nsIDOMHTMLDocument)) return;
+	
+	var win = doc.defaultView;
+	var host = doc.location.host;
+	
+	switch (host) {
+		case 'w.soundcloud.com':
+			var selector = '.playButton';
+			var btns = doc.querySelectorAll(selector);
+			break;
+		case 'soundcloud.com':
+			var selector = '.sc-button-play';
+			var btns = doc.querySelectorAll(selector);		
+			break;
+		default:
+			return; //SoundStorm does not support other hosts
+	}
+	Services.console.logStringMessage('SoundStorm - REM DIV');
+	Services.console.logStringMessage('SoundStorm - HOST PASS');
+
+	var js = win.wrappedJSObject;
+	
+	//if (js.soundStormMut) {
+	if (win in regedMuts) {
+		//js.soundStormMut.disconnect();
+		regedMuts[win].disconnect();
+		delete js.soundStormMut;
+		[].forEach.call(btns, function(btn) {
+			btn.removeEventListener('mouseover', mouseover, false);
+			btn.removeEventListener('mouseout', mouseout, false);
+		});
+		
+		Services.console.logStringMessage('SoundStorm - MUT DELETED and MOUSE EVENTS REMOVED');
+	}
+	
+	Services.console.logStringMessage('SoundStorm - remove success');
+	
+}
+
+function listenPageLoad(event) {
+	var win = event.originalTarget.defaultView;
+	var doc = win.document;
+	if (win.frameElement) {
+		//its a frame
+		//Cu.reportError('SoundStorm - a frame loaded');
+	}
+	Services.console.logStringMessage('SoundStorm - listPageLoad fired');
+	addDiv(doc);
+}
+
+/*start - windowlistener*/
+var windowListener = {
+	//DO NOT EDIT HERE
+	onOpenWindow: function (aXULWindow) {
+		// Wait for the window to finish loading
+		let aDOMWindow = aXULWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+		aDOMWindow.addEventListener("load", function () {
+			aDOMWindow.removeEventListener("load", arguments.callee, false);
+			windowListener.loadIntoWindow(aDOMWindow, aXULWindow);
+		}, false);
+	},
+	onCloseWindow: function (aXULWindow) {},
+	onWindowTitleChange: function (aXULWindow, aNewTitle) {},
+	register: function () {
+		// Load into any existing windows
+		let XULWindows = Services.wm.getXULWindowEnumerator(null);
+		while (XULWindows.hasMoreElements()) {
+			let aXULWindow = XULWindows.getNext();
+			let aDOMWindow = aXULWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+			windowListener.loadIntoWindow(aDOMWindow, aXULWindow);
+		}
+		// Listen to new windows
+		Services.wm.addListener(windowListener);
+	},
+	unregister: function () {
+		// Unload from any existing windows
+		let XULWindows = Services.wm.getXULWindowEnumerator(null);
+		while (XULWindows.hasMoreElements()) {
+			let aXULWindow = XULWindows.getNext();
+			let aDOMWindow = aXULWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+			windowListener.unloadFromWindow(aDOMWindow, aXULWindow);
+		}
+		//Stop listening so future added windows dont get this attached
+		Services.wm.removeListener(windowListener);
+	},
+	//END - DO NOT EDIT HERE
+	loadIntoWindow: function (aDOMWindow, aXULWindow) {
+		if (!aDOMWindow) {
+			return;
+		}
+		if (aDOMWindow.gBrowser) {
+			aDOMWindow.gBrowser.addEventListener('load', listenPageLoad, true);
+			if (aDOMWindow.gBrowser.tabContainer) {
+				//has tabContainer
+				//start - go through all tabs in this window we just added to
+				var tabs = aDOMWindow.gBrowser.tabContainer.childNodes;
+				for (var i = 0; i < tabs.length; i++) {
+					Cu.reportError('SoundStorm - DOING tab: ' + i);
+					var tabBrowser = tabs[i].linkedBrowser;
+					var win = tabBrowser.contentWindow;
+					loadIntoContentWindowAndItsFrames(win);
+				}
+				//end - go through all tabs in this window we just added to
+			} else {
+				//does not have tabContainer
+				var win = aDOMWindow.gBrowser.contentWindow;
+				loadIntoContentWindowAndItsFrames(win);
+			}
+		} else {
+			//window does not have gBrowser
+		}
+	},
+	unloadFromWindow: function (aDOMWindow, aXULWindow) {
+		if (!aDOMWindow) {
+			return;
+		}
+		if (aDOMWindow.gBrowser) {
+			aDOMWindow.gBrowser.removeEventListener('load', listenPageLoad, true);
+			if (aDOMWindow.gBrowser.tabContainer) {
+				//has tabContainer
+				//start - go through all tabs in this window we just added to
+				var tabs = aDOMWindow.gBrowser.tabContainer.childNodes;
+				for (var i = 0; i < tabs.length; i++) {
+					Cu.reportError('SoundStorm - DOING tab: ' + i);
+					var tabBrowser = tabs[i].linkedBrowser;
+					var win = tabBrowser.contentWindow;
+					unloadFromContentWindowAndItsFrames(win);
+				}
+				//end - go through all tabs in this window we just added to
+			} else {
+				//does not have tabContainer
+				var win = aDOMWindow.gBrowser.contentWindow;
+				unloadFromContentWindowAndItsFrames(win);
+			}
+		} else {
+			//window does not have gBrowser
+		}
+	}
+};
+/*end - windowlistener*/
+
+function loadIntoContentWindowAndItsFrames(theWin) {
+	var frames = theWin.frames;
+	var winArr = [theWin];
+	for (var j = 0; j < frames.length; j++) {
+		winArr.push(frames[j].window);
+	}
+	Cu.reportError('SoundStorm - # of frames in tab: ' + frames.length);
+	for (var j = 0; j < winArr.length; j++) {
+		if (j == 0) {
+			Cu.reportError('SoundStorm - **checking win: ' + j + ' location = ' + winArr[j].document.location);
+		} else {
+			Cu.reportError('SoundStorm - **checking frame win: ' + j + ' location = ' + winArr[j].document.location);
+		}
+		var doc = winArr[j].document;
+		//START - edit below here
+		//addDiv(doc); //note: noit //commented out so can test attaching on load
+		//break; //uncomment this line if you don't want to add to frames
+		//END - edit above here
+	}
+}
+
+function unloadFromContentWindowAndItsFrames(theWin) {
+	var frames = theWin.frames;
+	var winArr = [theWin];
+	for (var j = 0; j < frames.length; j++) {
+		winArr.push(frames[j].window);
+	}
+	Cu.reportError('SoundStorm - # of frames in tab: ' + frames.length);
+	for (var j = 0; j < winArr.length; j++) {
+		if (j == 0) {
+			Cu.reportError('SoundStorm - **checking win: ' + j + ' location = ' + winArr[j].document.location);
+		} else {
+			Cu.reportError('SoundStorm - **checking frame win: ' + j + ' location = ' + winArr[j].document.location);
+		}
+		var doc = winArr[j].document;
+		//START - edit below here
+		removeDiv(doc);
+		//break; //uncomment this line if you don't want to remove from frames
+		//END - edit above here
+	}
+}
+
+function startup(aData, aReason) {
+	windowListener.register();
+}
+
+function shutdown(aData, aReason) {
+	if (aReason == APP_SHUTDOWN) return;
+	windowListener.unregister();
+}
+
+function install() {}
+
+function uninstall() {}
