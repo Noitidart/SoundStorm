67ffea389d49662b023851fc8c3c43d8f5bda77c
Noitidart <noitidart@gmail.com>
2014-02-09 15:50:23 -0800

    initial.rev2
    bootstrap.js
    -todo: its not attaching always, need to fix this
    -todo: add hotkey to focus();
    -major outstanding issue right now: todo: is to get the mouse event to
    stop when toggling on a currently repeating and currently playing song
    (on stopPropagation its still throwing clicking so need to prevent it
    there too)
    
    -DONE: so right now the issue is how can i allow a user to click on
    another play button, it should pause the currently repeating one, and
    play the one they clicked, if they repeat that one that cRep is detached
    completely
    -DONE: other issue is how can i allow the user to pause the repeating
    track itself
    -DONE: other issue i found is that querySelector is not catching the
    soundcloud main player button (fixed: added ,.playControl to pattern)
    -DONE: had mispelled stopPropagation

--- bootstrap.js
+++ bootstrap.js
@@ -1,40 +1,258 @@
 const {interfaces: Ci,	utils: Cu} = Components;
 Cu.import('resource://gre/modules/Services.jsm');
+var cRep = {}; //object holding details of what is currently repeating //useufl for
+var tRep = {}; //temp object, for between mousedown and repeat funcs, object holding details of what is currently repeating //useufl for
 
 var mutConfig = {attributes: true};
 function mutFunc(ms) {
 	for (var m of ms) {
-		Services.console.logStringMessage('SoundStorm - mut fired att changed = "' + m.attributeName + '""');
+		Cu.reportError('SoundStorm - mut fired att changed = "' + m.attributeName + '""');
 		if (m.attributeName == 'class') {
 			var mClass = m.target.getAttribute('class');
-			Services.console.logStringMessage('SoundStorm - mClass Changed = "' + mClass + '""');
-			
-			var DOMWin = m.target.ownerDocument.defaultView.QueryInterface(Ci.nsIInterfaceRequestor)
-														   .getInterface(Ci.nsIWebNavigation)
-														   .QueryInterface(Ci.nsIDocShellTreeItem)
-														   .rootTreeItem
-														   .QueryInterface(Ci.nsIInterfaceRequestor)
-														   .getInterface(Ci.nsIDOMWindow);
-
-				Services.console.logStringMessage('SoundStorm - 100ms delayed');
-				if (!/(pause|playing)/.test(mClass)) {
-					DOMWin.setTimeout(function() {
-						Services.console.logStringMessage('SoundStorm - clicking now');
-						//click it to play it again
-						m.target.click();
-					}, 100);
-				}
+			Cu.reportError('SoundStorm - mClass Changed = "' + mClass + '""');
+			if (!/(pause|playing)/.test(mClass)) {
+				Cu.reportError('SoundStorm - clicking now');
+				//click it to play it again
+				m.target.click();
+			}
 			break;
 		}
 	}
 }
 
-function mouseover(e) {
-	e.target.style.transform = 'scale(3,3)';
+var heldTimer;
+var repeated;
+
+function focus() {
+	if (cRep.DOMWin) {
+		if (cRep.DOMWin.closed) {
+			Cu.reportError('SoundStorm - The DOMWin of the song has been closed, cannot focus anything');
+			return;
+		}
+		//cRep.DOMWin is meant to check if cRep is an empty object or not
+		//to test if DOMWin is closed do DOMWin.closed
+		//to test if tab is closed check tab.parentNode
+		//cRep.DOMWin.focus();
+		//ok up to this point we verify DOMWin is open
+		if (cWin.tab) { //check to see if theres a tab in cRep, if there is then this tells us there is obviously a tabContainer in this DOMWin
+			if (tab.parentNode) {
+				//cRep.DOMWin.gBrowser.selectedTab = tab;
+				//ok up to this point we verify that tab is open
+			} else {
+				//it had a tab but its now closed so return
+				Cu.reportError('SoundStorm - Tab that had song is no longer open, cannot focus anything');
+				return;
+			}
+		}
+		
+		if (btn.parentNode) { //im guessing this tells if btn exists or not
+			//ok we verify btn exists, so now focus it
+			cRep.DOMWin.focus();
+			if (cRep.tab) {
+				cRep.DOMWin.gBrowser.selectedTab = cRep.tab;
+			}
+			btn.scrollIntoView(true);
+			//todo: add downlaod style arrows pointing to the play button
+		} else {
+			Cu.reportError('SoundStorm - Btn is no longer there, cannot focus anything');
+			return;
+		}
+	}
 }
 
-function mouseout(e) {
-	e.target.style.transform = '';
+function repeat(e) {
+	repeated = true;
+	Cu.reportError('SoundStorm - Repeat triggered');
+	var label = tRep.btn.parentNode.querySelector('.soundStormLabel');
+
+	if (!label) {
+		Cu.reportError('SoundStorm - NO LABEL!!!!!!!!');
+		label.childNodes[0].innerHTML = 'Repeating..';
+	}
+	
+	if (cRep.btn == tRep.btn) {
+		//its currently repeating so turn repeat off
+		tRep.DOMWin.soundStormMut.disconnect();
+		cRep = {};
+	} else {
+		tRep.DOMWin.soundStormMut.disconnect();
+		tRep.DOMWin.soundStormMut.observe(tRep.btn, mutConfig);
+		cRep.DOMWin = tRep.DOMWin;
+		cRep.tab = tRep.tab;
+		cRep.win = tRep.win;
+		cRep.btn = tRep.btn;
+	}
+
+}
+
+function mousedown(e) {
+	var doc = e.target.ownerDocument;
+	var win = doc.defaultView;
+	
+	var btn = e.target;
+	var doc = e.target.ownerDocument;
+	var win = doc.defaultView;
+	var DOMWin = win.QueryInterface(Ci.nsIInterfaceRequestor)
+					.getInterface(Ci.nsIWebNavigation)
+					.QueryInterface(Ci.nsIDocShellTreeItem)
+					.rootTreeItem
+					.QueryInterface(Ci.nsIInterfaceRequestor)
+					.getInterface(Ci.nsIDOMWindow);
+	if (DOMWin.gBrowser) {
+		var tab = DOMWin.gBrowser._getTabForContentWindow(win);
+		/*
+		for (let i = 0; i < this.browsers.length; i++) {
+			if (this.browsers[i].contentWindow == aWindow)
+				return this.tabs[i];
+			}
+			return null;
+		}
+		*/
+		tRep.tab = tab;
+	}
+	
+	tRep.btn = btn;
+	tRep.win = win;
+	tRep.DOMWin = DOMWin;
+	mClassBetweenMouseupAndClick = null;
+	repeated = false;
+	heldTimer = tRep.DOMWin.setTimeout(repeat, 500);
+}
+
+function mouseup(e) {
+	if (!repeated) {
+		//do nothing
+		tRep.DOMWin.clearTimeout(heldTimer);
+		//start - this block here should allow user to pause even if its repeating
+		if (cRep.btn && cRep.btn.parentNode) {
+			if (cRep.btn == tRep.btn) { //check if cRep is not empty, then check if btn still exists, THEN check if cRep btn == tRep btn
+				if (/(pause|playing)/.test(cRep.btn.getAttribute('class'))) {
+					Cu.reportError('SoundStorm - it was playing will now be PAUSING');
+					cRep.DOMWin.soundStormMut.disconnect();
+				} else {
+					//its not playing
+					Cu.reportError('SoundStorm - it was paused will now be PLAYING');
+					cRep.DOMWin.soundStormMut.observe(cRep.btn, mutConfig);
+				}
+			//end - this block here should allow user to pause even if its repeating
+			} else { //start - this block here should allow user to play another one by clicking
+				//so tRep.btn (the user just clicked) is not == the cRep.btn which is last repeating
+				//must use tRep in this block obviously as it is not equal to cRep
+				if (/(pause|playing)/.test(tRep.btn.getAttribute('class'))) { //must use tRep.btn here
+					Cu.reportError('SoundStorm - it was playing will now be PAUSING');
+					tRep.DOMWin.soundStormMut.disconnect();
+				} else {
+					//its not playing
+					Cu.reportError('SoundStorm - it was paused will now do NOTHING as user is playing another by click');
+				}
+			} //end - this block here should allow user to play another one by clicking
+		}
+	} else {
+		var mClass = tRep.btn.getAttribute('class');
+		mClassBetweenMouseupAndClick = mClass;
+		Cu.reportError('mClass mouseup == "' + mClass + '"');
+		if (/(pause|playing)/.test(tRep.btn.getAttribute('class'))) {
+			//its playing so just toggle repeat
+			e.stopPropagation();
+			e.preventDefault();
+			e.returnValue = false;
+			Cu.reportError('SHOULD HAVE STOPPED MOUSEUP EVENT');
+		} else {
+			//do nothing let the click through so it plays
+		}
+		tRep.DOMWin.setTimeout(function() { Cu.reportError('mouseup timer fired repeated == ' + repeated); repeated = false }, 100); //set repeated to false after 100ms in case click event doesnt fire
+	}
+}
+
+var mClassBetweenMouseupAndClick;
+
+function click(e) {
+	if (repeated) {
+		Cu.reportError('click event fired while repeated == TRUE')
+		//var mClass = tRep.btn.getAttribute('class'); //cant do this here, need to read attr from when in mouseup, before it gets to click and after mouseup it already has the pause class
+		var mClass = mClassBetweenMouseupAndClick;
+		//Cu.reportError('mClass click == "' + mClass + '"');
+		Cu.reportError('mClass click but from mClassBetweenMouseupAndClick== "' + mClass + '"');
+		if (/(pause|playing)/.test(tRep.btn.getAttribute('class'))) {
+			//its playing so just toggle repeat
+			e.stopPropagation();
+			e.preventDefault();
+			e.returnValue = false;
+			Cu.reportError('SHOULD HAVE STOPPED CLICK EVENT');
+		} else {
+			//do nothing let the click through so it plays
+		}
+		repeated = false;
+	}
+}
+
+function mouseenter(e) {
+	//e.target.style.transform = 'scale(3,3)';
+	var doc = e.target.ownerDocument;
+	var win = doc.defaultView;
+	var btn = e.target;
+	
+	//test if btn is really the btn
+	var parentNode = btn.target;
+	var btnClass = btn.getAttribute('class');
+	if (!btnClass) {
+		Cu.reportError('SoundStorm - NOT BTN')
+		return;
+	}
+	if (/(sc-button-play|playButton)/.test(btnClass)) {
+		Cu.reportError('SoundStorm - YES its btn')
+	}
+	
+	var label = btn.parentNode.querySelector('.soundStormLabel');
+
+	if (!label) {
+		//btn.style.outline = '1px solid red';
+
+		var mydiv = doc.createElementNS('http://www.w3.org/1999/xhtml', 'div');
+		mydiv.style.width = btn.offsetWidth + 'px';
+		mydiv.style.height = btn.offsetHeight + 'px';
+		mydiv.style.position = 'absolute';
+		mydiv.style.top = btn.offsetTop + 'px';
+		mydiv.style.left = btn.offsetLeft + 'px';
+		mydiv.style.pointerEvents = 'none';
+		mydiv.style.display = 'flex';
+		mydiv.style.justifyContent = 'flex-start';
+		mydiv.style.alignItems = 'center';
+		mydiv.style.flexWrap = 'nowrap';
+		mydiv.style.overflow = 'visible';
+		mydiv.setAttribute('class','soundStormLabel');
+		
+		mydiv.innerHTML = '<div class="dialog sc-border-box g-z-index-overlay dialog__leftcenter" tabindex="-1" style="flex-shrink:0;z-index:900; background-color:white; outline: medium none; position: relative; left: 115%; border-radius: 3px; padding: 3px; text-shadow: 2px 1px 1px rgba(0, 0, 0, 0.05); font-size:14px; white-space:nowrap;">Hold to Repeat<div style="transform:rotate(45deg);top:-5px;left:45%;" class="dialog__arrow"></div></div>';
+
+		btn.parentNode.insertBefore(mydiv, btn);
+		
+		
+	} else {
+		label.style.display = 'flex';
+		label.childNodes[0].innerHTML = 'Hold to Repeat';
+	}
+}
+
+function mouseleave(e) {
+	//e.target.style.transform = '';
+	var doc = e.target.ownerDocument;
+	var win = doc.defaultView;
+	var btn = e.target;
+	
+	if (!repeated) {
+		if (tRep.DOMWin) {
+			tRep.DOMWin.clearTimeout(heldTimer);
+		}
+	}
+	
+	var label = btn.parentNode.querySelector('.soundStormLabel');
+
+	if (!label) {
+		Cu.reportError('SoundStorm - No label on mouseleave!')
+	} else {
+		label.style.display = 'none';
+	}
+	var btn = e.target;
 }
 
 var regedMuts = {};
@@ -45,8 +263,8 @@ function addDiv(doc) {
 
 	var win = doc.defaultView;
 	var host = doc.location.host;
-	Services.console.logStringMessage('SoundStorm - host = "' + host + '""');
-	Services.console.logStringMessage('SoundStorm - isFrame = "' + win.frameElement + '""');
+	Cu.reportError('SoundStorm - host = "' + host + '""');
+	Cu.reportError('SoundStorm - isFrame = "' + win.frameElement + '""');
 
 	
 	switch(host) {
@@ -55,33 +273,29 @@ function addDiv(doc) {
 			var btns = doc.querySelectorAll(selector);
 			break;
 		case 'soundcloud.com':
-			var selector = '.sc-button-play';
+			var selector = '.sc-button-play,.playControl';
 			var btns = doc.querySelectorAll(selector);		
 			break;
 		default:
 			return; //SoundStorm does not support other hosts
 	}
-	Services.console.logStringMessage('SoundStorm - ADD DIV');
-	Services.console.logStringMessage('SoundStorm - HOST NAME PASS');
+	Cu.reportError('SoundStorm - ADD DIV');
+	Cu.reportError('SoundStorm - HOST NAME PASS');
 	
-	var DOMWin = win.QueryInterface(Ci.nsIInterfaceRequestor)
-                         .getInterface(Ci.nsIWebNavigation)
-                         .QueryInterface(Ci.nsIDocShellTreeItem)
-                         .rootTreeItem
-                         .QueryInterface(Ci.nsIInterfaceRequestor)
-                         .getInterface(Ci.nsIDOMWindow);
-	
-	Services.console.logStringMessage('SoundStorm - # of btns on this page = ' + btns.length);
+	Cu.reportError('SoundStorm - # of btns on this page = ' + btns.length);
 	[].forEach.call(btns, function(btn, i) {
-		Services.console.logStringMessage('SoundStorm - loop ' + i);
-		btn.addEventListener('mouseover', mouseover, false);
-		btn.addEventListener('mouseout', mouseout, false);
-		DOMWin.soundStormMut.observe(btn, mutConfig);
+		Cu.reportError('SoundStorm - loop ' + i);
+		btn.addEventListener('mouseenter', mouseenter, false);
+		btn.addEventListener('mouseleave', mouseleave, false);
+		btn.addEventListener('mousedown', mousedown, true);
+		btn.addEventListener('mouseup', mouseup, true);
+		btn.addEventListener('click', click, true);
+		//DOMWin.soundStormMut.observe(btn, mutConfig);
 	});
 	
-	Services.console.logStringMessage('SoundStorm - MOUSE EVENTS ADDED');
+	Cu.reportError('SoundStorm - MOUSE EVENTS ADDED');
 	
-	Services.console.logStringMessage('SoundStorm - add success');
+	Cu.reportError('SoundStorm - add success');
 }
 
 function removeDiv(doc) {
@@ -97,35 +311,43 @@ function removeDiv(doc) {
 			var btns = doc.querySelectorAll(selector);
 			break;
 		case 'soundcloud.com':
-			var selector = '.sc-button-play';
+			var selector = '.sc-button-play,.playControl';
 			var btns = doc.querySelectorAll(selector);		
 			break;
 		default:
 			return; //SoundStorm does not support other hosts
 	}
-	Services.console.logStringMessage('SoundStorm - REM DIV');
-	Services.console.logStringMessage('SoundStorm - HOST PASS');
+	Cu.reportError('SoundStorm - REM DIV');
+	Cu.reportError('SoundStorm - HOST PASS');
 
 		
 	if (btns.length > 0) {
 		[].forEach.call(btns, function(btn) {
-			btn.removeEventListener('mouseover', mouseover, false);
-			btn.removeEventListener('mouseout', mouseout, false);
+			btn.removeEventListener('mouseenter', mouseenter, false);
+			btn.removeEventListener('mouseleave', mouseleave, false);
+			btn.removeEventListener('mousedown', mousedown, true);
+			btn.removeEventListener('mouseup', mouseup, true);
+			btn.removeEventListener('click', click, true);
+			
+			var label = btn.parentNode.querySelector('.soundStormLabel');
+			if (label) {
+				label.parentNode.removeChild(label);
+			}
 		});
 	}
 	
-	Services.console.logStringMessage('SoundStorm - remove success');
+	Cu.reportError('SoundStorm - remove success');
 	
 }
 
 function listenPageLoad(event) {
 	var win = event.originalTarget.defaultView;
-	var doc = win.document;
+	var doc = win.document; //note: noit: i got a win is not defined error here once but i didnt look into it 2/9/14 1235a
 	if (win.frameElement) {
 		//its a frame
 		//Cu.reportError('SoundStorm - a frame loaded');
 	}
-	Services.console.logStringMessage('SoundStorm - listPageLoad fired');
+	Cu.reportError('SoundStorm - listPageLoad fired');
 	addDiv(doc);
 }
 
@@ -238,8 +460,8 @@ function loadIntoContentWindowAndItsFrames(theWin) {
 		}
 		var doc = winArr[j].document;
 		//START - edit below here
-		//addDiv(doc); //note: noit //commented out so can test attaching on load
-		//break; //uncomment this line if you don't want to add to frames
+		addDiv(doc); //note: noit //commented out so can test attaching on load
+		//break; //comment this line if you don't want to add to frames
 		//END - edit above here
 	}
 }
@@ -260,7 +482,7 @@ function unloadFromContentWindowAndItsFrames(theWin) {
 		var doc = winArr[j].document;
 		//START - edit below here
 		removeDiv(doc);
-		//break; //uncomment this line if you don't want to remove from frames
+		//break; //comment this line if you don't want to remove from frames
 		//END - edit above here
 	}
 }
